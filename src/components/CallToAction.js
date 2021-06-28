ce6af16869e49c67eb83847ecfd2d71dbab435de
Gleb <gleb.rosev@itechart-group.com>
2021-06-28 20:57:03 +0300

    add tracking staff

---
+++ src/components/CallToAction.js
@@ -0,0 +1,6 @@
+const CallToAction = (props) => {
+  console.log(props);
+  return <h1>{props.laptop}</h1>;
+};
+
+export default CallToAction;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
+const client = new ApolloClient({
+  uri: "https://graphql.contentful.com/content/v1/spaces/1xdlbs34uw51?access_token=m2MzjcPA7PfSHOGQsgtA94xVhkKBUn30LYcHP_B0y4c",
+  cache: new InMemoryCache(),
+});
+
+render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
